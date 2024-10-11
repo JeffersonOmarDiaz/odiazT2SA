fe079453a0a54f10c556119b4a31da6249d4d1cd
master <master@SMLAB306PC15_25>
2024-10-10 20:39:59 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace odiazT2SA
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace odiazT2SA
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
